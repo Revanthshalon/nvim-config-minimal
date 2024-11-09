9fb2ab92ec32a1ed97e85b14ac20445abc3efead
Revanth Shalon Raj <revanth@Revanths-MacBook-Air.local>
2024-11-09 16:57:50 +0530

    Theme Update
    - Updated theme settings to show line number values

--- lua/revanth/plugins/theme.lua
+++ lua/revanth/plugins/theme.lua
@@ -1,20 +1,19 @@
 return {
-  'tanvirtin/monokai.nvim',
+  "folke/tokyonight.nvim",
+  lazy = false,
   priority = 1000,
-  config = function()
-    -- local transparent = true -- set to true if you would like to enable transparency
-    local config = require "monokai"
-
-    -- local bg = "#011628"
-    -- local bg_dark = "#011423"
-    -- local bg_highlight = "#143652"
-    -- local bg_search = "#0A64AC"
-    -- local bg_visual = "#275378"
-    -- local fg = "#CBE0F0"
-    -- local fg_dark = "#B4D0E9"
-    -- local fg_gutter = "#627E97"
-    -- local border = "#547998"
-
-    config.setup({})
-  end,
+  opts = {
+    style = "storm",
+    transparent = true,
+    cache = true,
+    terminal_colors = true,
+    on_colors = function(colors)
+      colors.fg_gutter = "#b2b8cf"
+    end
+  },
+  config = function(_, opts)
+    local tokyonight = require "tokyonight"
+    tokyonight.setup(opts)
+    tokyonight.load()
+  end
 }
