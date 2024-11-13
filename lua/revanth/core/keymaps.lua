d9e5333bb46126130049080c0fe60b7c9862cc32
Revanth Shalon Raj <revanth@Revanths-MacBook-Air.local>
2024-11-13 06:54:42 +0530

    Keymaps & Lsp Updates
    - Added Keymaps and disabled arrowkeys
    - Updated LSP for proper autocomplete
    - Updated lualine
    - added autopair (brackets)
    - added trouble for diagnostics

--- lua/revanth/core/keymaps.lua
+++ lua/revanth/core/keymaps.lua
@@ -5,4 +5,10 @@ local keymap = vim.keymap -- for conciseness
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
-keymap.set("n", "<leader>pv", ":Ex<CR>", { desc = "" })
+keymap.set("n", "<leader>pv", ":Ex<CR>", { desc = "Exit insert mode" })
+keymap.set("n", "Y", "y$", { desc = "Copy the entire line" })
+
+vim.api.nvim_set_keymap('i', '<Up>', '<NOP>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<Down>', '<NOP>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<Left>', '<NOP>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<Right>', '<NOP>', { noremap = true, silent = true })
--- lua/revanth/plugins/lsp.lua
+++ lua/revanth/plugins/lsp.lua
@@ -1,10 +1,66 @@
 return {
-	"neovim/nvim-lspconfig",
-	name = "lspconfig",
-	cmd = { "LspInfo", "LspInstall", "LspUninstall"},
-	event = { "BufReadPost", "BufNewFile" },
-	config = function () 
-		local config = require "lspconfig"
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/nvim-cmp",
+    'L3MON4D3/LuaSnip',
+    'saadparwaiz1/cmp_luasnip',
+  },
+  name = "lspconfig",
+  cmd = { "LspInfo", "LspInstall", "LspUninstall"},
+  event = { "BufReadPost", "BufNewFile" },
+  config = function () 
+    local cmp = require "cmp"
+    local cmp_lsp = require "cmp_nvim_lsp"
+    local capabilities = vim.tbl_deep_extend(
+      "force",
+      {},
+      vim.lsp.protocol.make_client_capabilities(),
+      cmp_lsp.default_capabilities())
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        end,
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+      }),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'nvim-lsp_signature_help'},
+        { name = 'luasnip' }, -- For luasnip users.
+      }, {
+          { name = 'buffer' },
+        })
+    })
+
+    vim.diagnostic.config({
+      -- update_in_insert = true,
+      float = {
+        focusable = true,
+        style = "minimal",
+        border = "rounded",
+        source = "always",
+        header = "",
+        prefix = "",
+      },
+    })
+
+    local config = require "lspconfig"
 
     local on_attach = function(client, bufnr)
       -- Enable inlay hints
@@ -19,8 +75,9 @@ return {
       end, { desc = "Toggle Inlay Hints" })
     end
 
-		config.rust_analyzer.setup({
+    config.rust_analyzer.setup({
       on_attach = on_attach,
+      capabilities = capabilities,
       settings = {
         ["rust-analyzer"] = {
           checkOnSave = {
@@ -34,8 +91,8 @@ return {
             typeHints = true,
             chainingHints = true,
           },
-				},
-			},
+        },
+      },
     })
-	end,
+  end,
 }
